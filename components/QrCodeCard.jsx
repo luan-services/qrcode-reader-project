ea454a2f6a9c871fe7501d0420cb6c217d241110
luan-services <luandotservices@gmail.com>
2025-05-27 00:35:15 -0300

    card-changes

--- components/QrCodeCard.jsx
+++ components/QrCodeCard.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 
 const QrCodeCard = ({imgSrc, titleText, bottomText}) => {
     return (
-        <div className="bg-white dark:bg-gray-800 p-4 shadow-lg max-w-80 max-h-120 rounded-lg">
+        <div className="bg-white dark:bg-gray-800 pb-8 p-4 shadow-lg max-w-80 max-h-160 rounded-xl">
             {/* && significa se existe, ex: se existe imgSrc ele renderiza a div <img>, caso o contrario não mostra nada, 
             os atributos são passados ao renderizar o componente, nesse caso, são passados no CardContainer*/}
-            {imgSrc &&
-            <img className="p-1 rounded-lg" src={imgSrc} alt="Card Image"></img>
+            {imgSrc && 
+                <div className="pb-4">
+                    <img className="rounded-xl" src={imgSrc} alt="Card Image"></img>
+                </div>
             }
             {titleText &&
-            <h3 className="dark:text-white text-slate-900 text-xl font-bold p-1 text-center"> 
-                {titleText}
-            </h3>
+                <h3 className="dark:text-white text-slate-900 text-xl font-bold p-1 text-center"> 
+                    {titleText}
+                </h3>
             }
             {bottomText &&
-            <div className="dark:text-slate-300 text-slate-500 p-1 text-center">
-                {bottomText}
-            </div>
+                <div className="dark:text-slate-300 text-slate-500 p-1 text-center">
+                    {bottomText}
+                </div>
             }
 
 
